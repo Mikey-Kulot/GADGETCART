95623d78eb7f7ce48dcfc12b7366d13f80178e83
Mhairs27 <mhos.cambay.up@phinmaed.com>
2025-03-10 19:24:17 +0800

    ListItems

---
+++ app/src/main/java/com/example/gadgetcart/ListItemsActivity.kt
@@ -0,0 +1,25 @@
+package com.example.gadgetcart
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gadgetcart.databinding.ActivityListBinding
+
+class ListItemsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
