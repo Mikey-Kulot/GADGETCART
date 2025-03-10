b43abf72d8f63f82d24a9aa6d508c9bf2c2b7577
Mhairs27 <mhos.cambay.up@phinmaed.com>
2025-03-10 23:04:50 +0800

    Cart Page

---
+++ app/src/main/java/com/example/gadgetcart/CartActivity.kt
@@ -0,0 +1,25 @@
+package com.example.gadgetcart
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gadgetcart.databinding.ActivityCartBinding
+
+class CartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
