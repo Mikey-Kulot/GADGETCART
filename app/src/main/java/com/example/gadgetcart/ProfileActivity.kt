aa584b9e04decd5087c6c017d671678a96a9b707
Mhairs27 <mhos.cambay.up@phinmaed.com>
2025-03-14 10:41:59 +0800

    Profile

--- app/src/main/java/com/example/gadgetcart/ProfileActivity.kt
+++ app/src/main/java/com/example/gadgetcart/ProfileActivity.kt
@@ -1,4 +1,61 @@
 package com.example.gadgetcart
 
-class ProfileActivity {
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gadgetcart.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+    private val PICK_IMAGE_REQUEST = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivProfilePicture.setOnClickListener {
+            openGallery()
+        }
+
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.btnLogout.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.btnOrderTracking.setOnClickListener {
+            val intent = Intent(this, OrderTrackingActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnMyOrders.setOnClickListener {
+            val intent = Intent(this, CartActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val imageUri: Uri? = data.data
+            binding.ivProfilePicture.setImageURI(imageUri)
+        }
+    }
 }
